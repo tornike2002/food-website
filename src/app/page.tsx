02a2be4f3d33912dd66079312710220cc5fcfc61
tornike2002 <toko.toka2000@gmail.com>
2024-05-19 19:35:30 +0400

    added basic structure for home components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
-"use client"
+"use client";
 import styled from "styled-components";
-
+import Image from "next/image";
+import Dine from "/public/assets/dine.svg";
 export default function Home() {
   return (
     <HomeHeader>
-      <h1>hello</h1>
+      <Image src={Dine} alt="Home logo" />
+      <div>
+        <h1>Exquisite dining since 1989</h1>
+        <p>
+          Experience our seasonal menu in beautiful country surroundings. Eat
+          the freshest produce from the comfort of our farmhouse.
+        </p>
+        <button>BOOK A TABLE</button>
+      </div>
     </HomeHeader>
   );
 }
-const HomeHeader =styled.header`
+const HomeHeader = styled.header`
   background: url("/assets/home-pizza.jpg") no-repeat;
   background-position: center center;
   background-size: cover;
   min-height: 100vh;
   width: 100%;
-`
+`;
